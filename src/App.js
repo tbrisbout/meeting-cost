12a0fb3f1d57f6fa4b0242b34cef5a012a34ae09
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2019-07-27 17:11:46 +0200

    Add first test with riteway-jest and testing-library/react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 const App = () => (
   <div className="App">
     <header>
+      <h1 data-testid="appTitle">Meeting Cost</h1>
     </header>
   </div>
 );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { assert } from 'riteway-jest';
+import { cleanup, render } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('<App/>', () => {
+  afterEach(cleanup);
+
+  const { getByTestId } = render(<App/>);
+
+  assert({
+    given: 'the application title',
+    should: 'be Meeting Cost',
+    actual: getByTestId('appTitle').textContent,
+    expected: 'Meeting Cost',
+  });
+})
